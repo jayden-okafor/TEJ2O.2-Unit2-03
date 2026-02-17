19f0a7ad0e3717390950099620b5750198e0b8fa
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-02-17 19:21:06 +0000

    changed python code pls

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,26 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Jayden Okafor
+Created on: Feb 2026
+This module is a Micro:bit MicroPython program that calculates the perimeter and area of a rectangle
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+display.clear()
+sleep(1)
+
+display.scroll("A rectangle has dimensions 5 cm & 3 cm")
+display.show(Image.HAPPY)
+sleep(0.5)
+display.clear()
+
+
+display.scroll("The perimeter would be: " + str(2 * (5 + 3)) + ' cm')
+display.show(Image.HAPPY)
+sleep(0.5)
+display.clear()
+
+display.scroll("The area would be: " + str(5 * 3) + ' cm^2')
+display.show(Image.HAPPY)
+sleep(0.5)
+display.clear()
