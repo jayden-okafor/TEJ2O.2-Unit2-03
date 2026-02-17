300f14c0cd224ea416772ecfc02e173805e50e85
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-02-17 18:58:35 +0000

    fesjrgbrjgjgtnjtgtjgntjgtjgtjgtgnjtngtjn fdfdfdfd# Please enter the commgtgtgtggtgtgtggtgttgtggtgtgit message for your changes. Lines starting
    change this file

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 MTHS All rights reserved
  *
  * Created by: Jayden Okafor
  * Created on: Feb 2026
  * This program is able to multiply, divide, subtract and divide numbers
 */
 
-basic.clearScreen();
-basic.pause(1000);
+basic.clearScreen()
+basic.pause(1000)
 
-basic.showString('5*3=' + (5*3).toString());
+basic.showString('5*3=' + (5*3).toString() + 'cm')
 
-basic.clearScreen();
-basic.pause(1000);
+basic.clearScreen()
+basic.pause(1000)
 
-basic.showString('2(5+3)=' + (2 * 5 + 3).toString());
+basic.showString('2(5+3)=' + (2 * (5 + 3)).toString() + 'cm')
 
-basic.clearScreen();
-basic.pause(1000);
+basic.clearScreen()
+basic.pause(1000)
