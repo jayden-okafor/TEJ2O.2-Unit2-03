3b001407337591edc62b569fde39f69068d1691b
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-02-17 13:13:02 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,10 +8,9 @@
 basic.clearScreen();
 basic.pause(1000);
 
-
 basic.showString('5*3=' + (5*3).toString());
 
 basic.clearScreen();
 basic.pause(1000);
 
-basic.showString('5*3=' + (2 * 5 + 3).toString());
+basic.showString('2(5+3)=' + (2 * 5 + 3).toString());
